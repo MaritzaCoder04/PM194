fcc2ef76b4a45d534302dc7cf80c5bab5504bc62
Maritza <122042671@upq.edu.mx>
2025-06-13 20:27:06 -0600

    Flex2

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,23 +1,27 @@
 /* Zona 1 : Importaciones */
 
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React,{useState} from 'react';
 
+
 const Texto=({style})=>{
-  const [contenido,setContenido]=useState('Hola Mundo RNative')//contenido ya tiene un valor de arranque 
+  const [contenido,setContenido]=useState('Hola Mundo RNative');//contenido ya tiene un valor de arranque
   const actualizaTexto=()=>{setContenido('Estado actualizado del Text')} //funcion arrow :)
   return (
     <Text style={[styles.text, style]} onPress={actualizaTexto}> {contenido} </Text>//como es arrow no se ponen parentesis
   )
 };
 
+
 /* Zona 2 : Main */
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto style={styles.red} ></Texto>
+      <Texto style={styles.red}></Texto>
       <Texto style={styles.blue}></Texto>
       <Texto style={styles.green}></Texto>
       <StatusBar style="auto" />
@@ -25,19 +29,21 @@ export default function App() {
   );
 }
 
+
 /* Zona 3 : Estetica del screen */
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'baseline',
-    flexDirection: 'row',
-    justifyContent: 'centers'
+    justifyContent: 'center',
+    flexDirection:'row-reverse'
   },
   text:{
     color:'white',
-    fontSize:27
+    fontSize:27,
   },
   red:{backgroundColor:'red'},
   blue:{backgroundColor:'blue'},
